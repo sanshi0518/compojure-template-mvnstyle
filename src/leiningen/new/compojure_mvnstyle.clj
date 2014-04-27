8142c68b0de4d9fac8e7863a2c31402d67b0c5ec
wanglei <sanshi0518@163.com>
2014-04-27 23:36:50 +0800

    add test handler.clj

--- src/leiningen/new/compojure_mvnstyle.clj
+++ src/leiningen/new/compojure_mvnstyle.clj
@@ -7,7 +7,7 @@
   "Create a new maven style compojure project"
   [name]
   (let [data {:name name
-              :namespace (string/replace name #"(\w)+-" "")
+              :namespace (string/replace name #"\w+-" "")
               :year (year)}
         render #((renderer "compojure_mvnstyle") % data)]
     (main/info "Generating fresh 'lein new' compojure-mvnstyle project.")
@@ -16,6 +16,7 @@
              ["project.clj" (render "project.clj")]
              ["README.md"   (render "README.md")]
              ["src/main/clojure/{{namespace}}/handler.clj" (render "handler.clj")]
+             ["src/test/clojure/{{namespace}}/test/handler.clj" (render "handler_test.clj")]
              ["src/main/resources/public/js/bootstrap.min.js" (render "bootstrap.min.js")]
              ["src/main/resources/public/js/jquery-1.9.1.min.js" (render "jquery-1.9.1.min.js")]
              ["src/main/resources/public/css/bootstrap.min.css" (render "bootstrap.min.css")]
